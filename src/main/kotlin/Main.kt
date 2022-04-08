96102216f00c2790037ce7375c5a2d8be1351726
marcio <viana_marcio@outlook.com>
2022-04-08 13:48:57 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main() { // this is the first line
+    println("**********************")
+    println("**********************")
+    println("***** F E L I Z ******")
+    println("A N I V E R S Á R I O ")
+    println("     M A R C O S      ")
+    println("    R O G É R I O     ")
+    println("**********************")
+    println("**********************")
+}
